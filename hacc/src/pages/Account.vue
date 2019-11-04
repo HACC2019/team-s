a932cf2bafea198e2f5d09403d451824972e5071
Sum-Tim <timsumalinogiii@gmail.com>
2019-11-03 15:50:22 -1000

    still working

--- hacc/src/pages/Account.vue
+++ hacc/src/pages/Account.vue
@@ -1,22 +1,38 @@
 <template>
     <div id="account-main">
        <div class="row">
-            <div class="col s7 push-s5"><span class="flow-text">This div is 7-columns wide on pushed to the right by 5-columns.</span></div>
-            <div class="col s5 pull-s7"><span class="flow-text">5-columns wide pulled to the left by 7-columns.</span></div>
+            <div class="col s7 push-s5"><span class="flow-text">This div is 7-columns wide on pushed to the right by 5</span></div>
+            <div class="col s5 pull-s7"><span class="flow-text">Dashloha</span></div>
        </div>
 
 
        <div class="row">
 
             <div class="col s9 push-s2 blocky">
-               <div class="row blockyRow">
+                <div id="backBlock"></div>
+               <div class="row blockyRow offset">
                     <div class="col s1"></div>
-                    <div class="col s12 profileBlock"></div>
+
+                    <div class="col s12 profileBlock">
+                        <div class="col s11 center offset">
+                            <div class="row">Profile</div>
+                            <div class="row"></div>
+                            <div class="row"></div>
+                        </div>
+                    </div>
+
                     <div class="col s1"></div> 
-                    <div class="col s12 profileBlock">ssdssdd</div>
+
+                    <div class="col s12">
+                        <div class="row profileBlockSmall">qwe</div>
+                        <div class="row profileBlockSmall">wef</div>
+                    </div>
+
                     <div class="col s1"></div>
                </div>
             </div>
+
+            <div id="hello"></div>
 
             <!--This is the side bar-->
             <div class="col s2 pull-s10 sideBar">
@@ -29,14 +45,34 @@
     </div>
 </template>
 
+<script>
+export default {
+    components: {
+
+    },
+    data: function() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
 <style scoped>
+#account-main {
+}
+
 .blocky {
-    background-color: white;
+    background-color: rgba(34, 34, 34, 0.4);
     border: solid 1px black;
     border-radius: 25px;
-    height:  50rem;
-    opacity: 0.1;
+    height:  50rem;   
+    position: relative;
+    z-index: -2;
 }
+
+
 
 .blockyRow {
     display: flex;
@@ -44,11 +80,23 @@
 }
 
 .profileBlock {
-    background-color: white;    
+    background-color:white;    
     border: solid 1px black;
     border-radius:  25px;
-    height: 44rem;
-    margin-bottom: 3rem;
-    margin-top: 3rem;    
+    height: 44rem;         
 }
+
+
+.offset {
+    margin-bottom: 3rem;
+    margin-top: 3rem;  
+}
+.profileBlockSmall  {
+    background-color: white;
+    border: solid 1px black;
+    border-radius:  25px;
+    height: 21.4rem;
+}
+
+
 </style>

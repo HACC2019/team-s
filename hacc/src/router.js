c9a5bc5f1752989e3cdbbaec578d1a5c3bd1f644
caslabs <jeraldydcascayan@gmail.com>
2019-11-03 16:36:47 -1000

    Initialize Pages:login and register with basic routes

--- hacc/src/router.js
+++ hacc/src/router.js
@@ -1,4 +1,8 @@
 import Landing from './pages/Landing.vue'
+import Login from './pages/Login.vue'
+import Register from './pages/Register.vue'
 export default [
-    { path:'/', component: Landing}
+    { path:'/', component: Landing},
+    {path:'/login', component: Login},
+    {path:'/register', component: Register}
 ]

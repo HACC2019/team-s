4673521f7117d2763f03e103558f7da84b39a6a4
Alex <eieghtbitdolphin@gmail.com>
2019-11-03 16:00:06 -1000

    almost done with the landing page

--- hacc/src/pages/Landing.vue
+++ hacc/src/pages/Landing.vue
@@ -20,10 +20,108 @@
       </div>
 
       <!-- Section 2 -->
-      <div class="Section container" id="Section-Two">
+      <div class="Section" id="Section-Two">
         <div class="container Inner-Content-Container">
+          
+          <div class="row" style="color:#fcfcfc;">
+            <div class="col s12">
+              <h3>Our features</h3>
+              <h5>For the betterment of the community</h5>
+            </div>
+          </div>
+
+          <div class="row">
+            <template v-for="items in cards">
+              <div class="col s4" :key="items.title">
+                <div class="card Begin-Cards" style="padding-top:1rem;">
+                  <div class="card-image">
+                    <img :src="items.img" style="height:150px" alt="">
+                  </div>
+
+                  <div class="card-content">
+                    <p>{{items.text}}</p>
+                  </div>
+
+                </div>
+            </div>
+            </template>
+          </div>
+
         </div>
       </div>
+
+      <!-- Section Three-->
+      <div class="Section" id="Section-Three" style="color:#fcfcfc;">
+        <div class="container Inner-Content-Container">
+          <div class="row valign-wrapper" style="margin-bottom:0px;">
+            <div class="col s12 m7">
+              
+              <div class="row">
+
+                <div class="col s12">
+                  <h3>Change your community</h3>
+                </div>
+
+                <div class="col s11">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio libero, tempus ac rhoncus eu, pellentesque ut risus. Sed suscipit imperdiet lobortis. Nullam pretium feugiat nibh, eget dignissim ipsum convallis nec. Etiam vulputate, nunc id posuere ultrices, ipsum risus sodales mauris, vitae vulputate nisl ex ut urna. Mauris aliquet elit nec enim porttitor lacinia. Duis vitae aliquam nisl, et tincidunt magna. Suspendisse lacinia malesuada risus ac lacinia.
+
+Mauris eget sagittis nunc. Vivamus nec rhoncus lacus, sed malesuada velit. Phasellus porta turpis enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis purus felis, venenatis vitae ante et, vulputate pellentesque est. Cras ac dolor arcu. Cras eget lobortis augue. Nullam congue ornare euismod. Praesent interdum sem at turpis convallis gravida. Phasellus vitae ipsum eu felis finibus accumsan. Nullam vulputate faucibus urna at euismod. Cras tristique, mauris a efficitur vestibulum, nulla odio consectetur nibh, dapibus fermentum enim justo vestibulum dolor.</p>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="col s12 m5">
+              <img :src="require('@/Images/group.svg')" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Section Four -->
+      <div class="Section" id="Section-Four" style="color:#fcfcfc;">
+        <div class="container Inner-Content-Container">
+          <div class="row valign-wrapper" style="margin-bottom:0px;">
+            <div class="col s12 m5">
+              <img :src="require('@/Images/hawaii.svg')" alt="">
+            </div>
+            <div class="col s12 m7">
+              
+              <div class="row">
+
+                <div class="col s12 right">
+                  <h3>Aloha+ Challenge</h3>
+                </div>
+
+                <div class="col s11 right">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio libero, tempus ac rhoncus eu, pellentesque ut risus. Sed suscipit imperdiet lobortis. Nullam pretium feugiat nibh, eget dignissim ipsum convallis nec. Etiam vulputate, nunc id posuere ultrices, ipsum risus sodales mauris, vitae vulputate nisl ex ut urna. Mauris aliquet elit nec enim porttitor lacinia. Duis vitae aliquam nisl, et tincidunt magna. Suspendisse lacinia malesuada risus ac lacinia.
+
+Mauris eget sagittis nunc. Vivamus nec rhoncus lacus, sed malesuada velit. Phasellus porta turpis enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis purus felis, venenatis vitae ante et, vulputate pellentesque est. Cras ac dolor arcu. Cras eget lobortis augue. Nullam congue ornare euismod. Praesent interdum sem at turpis convallis gravida. Phasellus vitae ipsum eu felis finibus accumsan. Nullam vulputate faucibus urna at euismod. Cras tristique, mauris a efficitur vestibulum, nulla odio consectetur nibh, dapibus fermentum enim justo vestibulum dolor.</p>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="Section" id="Section-Five">
+        <div class="container Inner-Content-Container valign-wrapper" style="height:100%;">
+          <div class="row">
+            <div class="col s12" style="text-align:center;">
+              <h2>Contrubute to the Community</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio libero, <br/>tempus ac rhoncus eu, pellentesque ut risus.</p>
+            </div>
+
+            <div class="col s12" style="text-align: center; margin-top: 2rem;">
+              <a class="waves-effect waves-light btn Intro-Button">Sign Up</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
 </template>
 
@@ -36,9 +134,9 @@ export default {
     data() {
       return {
         cards: [
-          {title:'Community Connect',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis vestibulum justo eget aliquam.'},
-          {title:'Local Projections', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis vestibulum justo eget aliquam.'},
-          {title: 'Visual Cues',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis vestibulum justo eget aliquam.'}
+          {title:'Community Connect',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis vestibulum justo eget aliquam.', img: require('@/Images/tree.svg')},
+          {title:'Local Projections', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis vestibulum justo eget aliquam.', img: require('@/Images/graph.svg')},
+          {title: 'Visual Cues',text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis vestibulum justo eget aliquam.', img: require('@/Images/newspaper.svg')}
         ],
       }
     }
@@ -62,9 +160,12 @@ export default {
   }
 
 
+
   .Section {
-    height: 80%;
+    position: relative;
+    display: block;
     z-index: 1;
+    height: 80%;
   }
 
   .Intro-Button {
@@ -76,6 +177,7 @@ export default {
     font-size: 6rem;
     color: #f6f6f6;
   }
+
 
   .Info-Card {
     width: 20rem;
@@ -93,11 +195,24 @@ export default {
     text-align: right;
   }
 
-  #Section-One {
+  #Section-One, #Section-Four {
     background-color: #d7263b;
   }
 
   #Section-Two {
     background-color: #9ae3d7;
+  }
+
+  #Section-Three {
+    background-color: #93ff96;
+  }
+
+  #Section-Four {
+    text-align: right;
+  }
+
+  .Begin-Cards {
+    overflow: hidden;
+    border-radius: 0.75rem;
   }
 </style>

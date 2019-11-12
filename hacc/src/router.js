35f072695cd3b44651bb009d650c2e7b5c5c47a0
Jeraldy <jeraldy@kuure.com>
2019-11-12 08:38:31 -1000

    routes fixed

--- hacc/src/router.js
+++ hacc/src/router.js
@@ -4,7 +4,7 @@ import Landing from './pages/Landing.vue'
 import Login from './pages/Login.vue'
 import Register from './pages/Register.vue'
 export default [
-    { path:'/', component: Account},
+    { path:'/account', component: Account},
     { path:'/', component: Landing},
     {path:'/login', component: Login},
     {path:'/register', component: Register}
